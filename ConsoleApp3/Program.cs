183edc5865deb4577d23621c52880577b83da415
iaguilar313 <iaguilar@umich.edu>
2017-09-27 15:29:12 -0400

    first update

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramContinue = true;
+
+            while (ProgramContinue == true)
+            {
+                int Input;
+                Console.WriteLine("Learn Your Squares and Cubes");
+                Console.WriteLine("Please Enter A Integer");
+                Input = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number" + "\t" + "Square" + "\t" + "Cubed");
+
+                for (int i = 1; i <= Input; i++)
+                    // for (int i = input; i>=1;i--)
+
+                {
+                    Console.WriteLine(i + "\t" + Math.Pow(i, 2) + "\t" + Math.Pow(i, 3));
+                }
+
+                string UserChoice;
+                Console.WriteLine("Do you want to run the code again? (N/No/y/yes)");
+                UserChoice = Console.ReadLine();
+                if (UserChoice == "N" || UserChoice == "No")
+                {
+                    ProgramContinue = false;
+                    Console.WriteLine("Bye");
+                }
+            }
+        }
+    }
+}
